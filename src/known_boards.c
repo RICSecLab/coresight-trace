7513de57378f34ea862b143b6200f93be43ab333
Akira Moroo <retrage01@gmail.com>
2021-06-11 07:37:18 +0000

    boards: Add Jetson Nano to known boards
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/known_boards.c
+++ src/known_boards.c
@@ -51,11 +51,109 @@ static int do_registration_thunderx2(struct cs_devices_t *devices)
     return 0;
 }
 
+static int do_registration_jetson_nano(struct cs_devices_t *devices)
+{
+  enum { A57_0, A57_1, A57_2, A57_3 };
+
+  int i;
+  cs_device_t funnel_a57, funnel_major, etf, rep, etr, tpiu, stm, sys_cti;
+
+  cs_register_romtable(0x72000000);
+
+  /* CTI affinities */
+  cs_device_set_affinity(cs_device_register(0x73420000), A57_0);
+  cs_device_set_affinity(cs_device_register(0x73520000), A57_1);
+  cs_device_set_affinity(cs_device_register(0x73620000), A57_2);
+  cs_device_set_affinity(cs_device_register(0x73720000), A57_3);
+
+  /* PMU affinities */
+  cs_device_set_affinity(cs_device_register(0x73430000), A57_0);
+  cs_device_set_affinity(cs_device_register(0x73530000), A57_1);
+  cs_device_set_affinity(cs_device_register(0x73630000), A57_2);
+  cs_device_set_affinity(cs_device_register(0x73730000), A57_3);
+
+  /* ETM affinities */
+  cs_device_set_affinity(cs_device_register(0x73440000), A57_0);
+  cs_device_set_affinity(cs_device_register(0x73540000), A57_1);
+  cs_device_set_affinity(cs_device_register(0x73640000), A57_2);
+  cs_device_set_affinity(cs_device_register(0x73740000), A57_3);
+
+  funnel_a57 = cs_device_get(0x73010000);
+  cs_atb_register(cs_cpu_get_device(A57_0, CS_DEVCLASS_SOURCE), 0,
+                  funnel_a57, 0);
+  cs_atb_register(cs_cpu_get_device(A57_1, CS_DEVCLASS_SOURCE), 0,
+                  funnel_a57, 1);
+  cs_atb_register(cs_cpu_get_device(A57_2, CS_DEVCLASS_SOURCE), 0,
+                  funnel_a57, 2);
+  cs_atb_register(cs_cpu_get_device(A57_3, CS_DEVCLASS_SOURCE), 0,
+                  funnel_a57, 3);
+
+  funnel_major = cs_device_get(0x72010000);
+  etf = cs_device_get(0x72030000);
+  rep = cs_device_get(0x72040000);
+  etr = cs_device_get(0x72050000);
+  tpiu = cs_device_get(0x72060000);
+  stm = cs_device_get(0x72070000);
+
+  cs_atb_register(funnel_a57, 0, funnel_major, 0);
+  cs_atb_register(stm, 0, funnel_major, 3);
+
+  cs_atb_register(funnel_major, 0, etf, 0);
+  cs_atb_register(etf, 0, rep, 0);
+  cs_atb_register(rep, 0, etr, 0);
+  cs_atb_register(rep, 1, tpiu, 0);
+
+  devices->itm = stm;
+  devices->etb = etf;
+
+  cs_stm_config_master(stm, 0, 0x71000000);
+  cs_stm_select_master(stm, 0);
+
+  /* etf */
+  sys_cti = cs_device_register(0x72020000);
+  cs_cti_connect_trigsrc(etf, CS_TRIGOUT_ETB_FULL,
+      cs_cti_trigsrc(sys_cti, 0));
+  cs_cti_connect_trigsrc(etf, CS_TRIGOUT_ETB_ACQCOMP,
+      cs_cti_trigsrc(sys_cti, 1));
+  cs_cti_connect_trigdst(cs_cti_trigdst(sys_cti, 0),
+      etf, CS_TRIGIN_ETB_TRIGIN);
+  cs_cti_connect_trigdst(cs_cti_trigdst(sys_cti, 1),
+      etf, CS_TRIGIN_ETB_FLUSHIN);
+
+  /* etr */
+  cs_cti_connect_trigsrc(etr, CS_TRIGOUT_ETB_FULL,
+      cs_cti_trigsrc(sys_cti, 2));
+  cs_cti_connect_trigsrc(etr, CS_TRIGOUT_ETB_ACQCOMP,
+      cs_cti_trigsrc(sys_cti, 3));
+  cs_cti_connect_trigdst(cs_cti_trigdst(sys_cti, 2),
+      etr, CS_TRIGIN_ETB_TRIGIN);
+  cs_cti_connect_trigdst(cs_cti_trigdst(sys_cti, 3),
+      etr, CS_TRIGIN_ETB_FLUSHIN);
+
+  /* stm */
+  cs_cti_connect_trigsrc(stm, CS_TRIGOUT_STM_ASYNCOUT,
+      cs_cti_trigsrc(sys_cti, 4));
+  cs_cti_connect_trigsrc(stm, CS_TRIGOUT_STM_TRIGOUTSPTE,
+      cs_cti_trigsrc(sys_cti, 5));
+  cs_cti_connect_trigsrc(stm, CS_TRIGOUT_STM_TRIGOUTSW,
+      cs_cti_trigsrc(sys_cti, 6));
+
+  for (i = 0; i < 4; i++) {
+    devices->cpu_id[i] = cpu_id[i];
+  }
+
+  return 0;
+}
+
 const struct board known_boards[] = {
   {
     .do_registration = do_registration_thunderx2,
     .n_cpu = 32, /* XXX: To limit initialized CPU ETMs */
     .hardware = "Marvell ThunderX2",
+  }, {
+    .do_registration = do_registration_jetson_nano,
+    .n_cpu = 4,
+    .hardware = "Jetson Nano",
   },
   {},
 };
